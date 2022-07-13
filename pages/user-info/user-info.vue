808ae7aacb93be0fbf3f408c97686a5e595992f7
Wea3el <wesleyz2004@gmail.com>
2022-07-12 23:16:15 -0400

    work
    work

---
+++ pages/user-info/user-info.vue
@@ -0,0 +1,42 @@
+<template>
+	<view>
+		<uni-list-item title="头像"
+		<image slot="right" src= "../../static/Dr. Von Neumann.jpeg" 
+		style="width: 150rpx;height: 150rpx; "
+		class="rounded-circle"></image>></uni-list-item>
+		<uni-list-item title="昵称"></uni-list-item>
+		<uni-list-item title="性别"></uni-list-item>
+		<uni-list-item title="生日"></uni-list-item>
+		<uni-list-item title="情感"></uni-list-item>
+		<uni-list-item title="职业"></uni-list-item>
+		<uni-list-item title="家乡"></uni-list-item>
+		
+		<view class ="py-2 px-3">
+			<button class="bg-main text-white" style ="border-radius: 50rpx;border:0;" 
+			type="primary" @click="submit">完成</button>
+		</view>
+		
+	</view>
+</template>
+
+<script>
+	import uniListItem from '@/uni-ui/uni-list-item/uni-list-item.vue'
+	export default {
+		components:{
+			uniListItem
+		
+		},
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
---
+++ pages/user-set/user-set.vue
@@ -0,0 +1,41 @@
+<template>
+	<view>
+		<uni-list-item title="账号与安全" @click="open('user-password')"></uni-list-item>
+		<uni-list-item title="绑定邮箱"@click="open('user-email')"></uni-list-item>
+		<uni-list-item title="资料编辑"@click="open('user-info')"></uni-list-item>
+		<uni-list-item title="清除缓存"></uni-list-item>
+		<uni-list-item title="意见反馈"></uni-list-item>
+		<uni-list-item title="关于社区"></uni-list-item>
+		<view class ="py-2 px-3">
+			<button class="bg-main text-white" style ="border-radius: 50rpx;border:0;" type="primary">退出登录</button>
+		</view>
+	</view>
+</template>
+
+<script>
+	import uniListItem from'@/uni-ui/uni-list-item/uni-list-item.vue'
+	export default {
+		components:{
+			uniListItem
+		},
+		
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			open(path){
+				uni.navigateTo({
+					url: `../${path}/${path}`
+				});
+			}
+			
+		}
+	}
+</script>
+
+
+<style>
+
+</style>
